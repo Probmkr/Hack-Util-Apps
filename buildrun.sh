1a714e52ef5fe601fc0b03f8071caf5c7eb8cd8b
Probmkr <Thanatos9911@gmail.com>
2022-05-21 09:47:37 +0900

    起動コマンドを少し修正

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+yarn build
+read -p "enter to continue: "
 screen -UAmdS builded yarn start
+
+if [ $1 == "-a" ]; then
+    screen -r builded
+fi
 
--- devrun.sh
+++ devrun.sh
@@ -2,3 +2,7 @@
 
 screen -UAmdS builded yarn dev
 
+if [ $1 == "-a" ]; then
+    screen -r dev
+fi
+
