793a9f904b82da13570162199d42bcd843a65f16
Probmkr <Thanatos9911@gmail.com>
2022-06-07 17:49:59 +0900

    nextjs-progress を有効化（前回何故かできてなかった）

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
+import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.scss";
-//import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/*<NextNprogress
+      <NextNProgress
         color="#55d"
         startPosition={0.3}
         stopDelayMs={200}
         height={3}
         showOnShallow={true}
-      />*/}
+      />
       <Component {...pageProps} />
     </>
   );
