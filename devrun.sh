339fc84cd2bdab7f7aadebe50ba942643ab238ab
Probmkr <Thanatos9911@gmail.com>
2022-06-07 20:39:01 +0900

    起動スクリプトを https 対応 起動ユーザーをデフォルトに戻した

--- devrun.sh
+++ devrun.sh
@@ -4,12 +4,12 @@ screen -ls dev > /dev/null
 if [ $? == 0 ]; then
     screen -S dev -X quit
 fi
-if [ $2 == 'onlykill' ]; then
+if [ $1 == 'onlykill' ]; then
     screen -ls
     exit 0
 fi
 
-screen -UAmdS dev yarn dev
+screen -UAmdS dev yarn ${1:-dev:https}
 screen -ls
 
 read -p "do you want to attach? (y:any): " attach
