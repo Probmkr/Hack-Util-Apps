23afa31a35261f332488e278fe0042bf5a1a648d
Probmkr <Thanatos9911@gmail.com>
2022-06-07 20:17:13 +0900

    ssl 化

--- src/lib/canvas/createTextLines.tsx
+++ src/lib/canvas/createTextLines.tsx
@@ -5,14 +5,13 @@ interface SeparatedText {
   remaining: string;
 }
 
-const createTextLine = (canvas: Canvas, text: string): SeparatedText => {
+const createTextLine = (canvas: Canvas, text: string, maxWidth: number): SeparatedText => {
   const context = canvas.getContext("2d");
-  const MAX_WIDTH = 1000 as const;
 
   for (let i = 0; i < text.length; i += 1) {
     const line = text.substring(0, i + 1);
 
-    if (context.measureText(line).width > MAX_WIDTH) {
+    if (context.measureText(line).width > maxWidth) {
       return {
         line,
         remaining: text.substring(i + 1),
@@ -26,12 +25,12 @@ const createTextLine = (canvas: Canvas, text: string): SeparatedText => {
   };
 };
 
-const createTextLines = (canvas: Canvas, text: string): string[] => {
+const createTextLines = (canvas: Canvas, text: string, maxWidth: number = 1000): string[] => {
   const lines: string[] = [];
   let currentText = text;
 
   while (currentText !== "") {
-    const separatedText = createTextLine(canvas, currentText);
+    const separatedText = createTextLine(canvas, currentText, maxWidth);
     lines.push(separatedText.line);
     currentText = separatedText.remaining;
   }
