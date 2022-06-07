67792f39d8ea3dd52edb408d56de447db7a73c30
Probmkr <Thanatos9911@gmail.com>
2022-06-07 17:41:47 +0900

    Revert "プログレスバーを有効化"
    This reverts commit ba257734d737c85078334a9afc17ba481ea7ed10.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.scss";
+//import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextNProgress
+      {/*<NextNprogress
         color="#55d"
         startPosition={0.3}
         stopDelayMs={200}
         height={3}
         showOnShallow={true}
-      />
+      />*/}
       <Component {...pageProps} />
     </>
   );
