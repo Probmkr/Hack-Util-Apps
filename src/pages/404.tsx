e6f01df5503b9dd9e2712e430f80a28989da4070
Probmkr <Thanatos9911@gmail.com>
2022-06-08 18:03:50 +0900

    ジョークを追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,9 @@ export default function My404() {
   return (
     <Layout pageTitle="404">
       <h1>404</h1>
-      <p>お探しのページは見つかりませんでした。</p>
+      <p>おいそこの君。 URL 直で入力しただろw</p>
+      <p>自分をあまり過信するなよww</p>
+      <p>（なお、管理者は自分のミスの可能性もあるとは考えないようだ。）</p>
     </Layout>
   );
 }
